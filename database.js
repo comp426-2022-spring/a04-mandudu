003d89fafb785ade6a7b589587c2be1f684e189b
mandudu <35313236+mandudu@users.noreply.github.com>
2022-04-14 22:46:55 -0400

    asdf

--- database.js
+++ database.js
@@ -2,11 +2,13 @@ const Database = require('better-sqlite3');
 
 const db = new Database('log.db');
 
-const stmt = db.prepare(`SELECT name FROM sqlite_master WHERE type = 'table' and name = 'accesslog';`);
+const statement = db.prepare(`SELECT name FROM sqlite_master WHERE type = 'table' and name = 'accesslog';`);
 
-let row = stmt.get();
+let row = statement.get();
 
 if (row == undefined) {
+    console.log('Log database is empty. Creating a log database...')
+    
     const sqlInit = `
         CREATE TABLE accesslog (
             accesslogid INTEGER PRIMARY KEY, 
