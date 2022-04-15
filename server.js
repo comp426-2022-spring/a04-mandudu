ecdaf3a8d498728298d1d29b9b9e89a2c51c403e
mandudu <35313236+mandudu@users.noreply.github.com>
2022-04-14 22:29:33 -0400

    asdf

--- server.js
+++ server.js
@@ -20,6 +20,47 @@ app.get("/app/", (req,res,next) => {
     res.json({"message": "Your API works! (200)"});
     res.status(200);
 });
+if(args.log == 'true'){
+    const access = fs.createWriteStream('access.log', {
+        flags: 'a'
+    }) 
+    app.use(morgan('combined', {stream: access}))
+}
+else {
+    console.log("NOTICE: not creating file acces.log");
+}
+
+const help = (`
+server.js [options]
+--port	Set the port number for the server to listen on. Must be an integer
+            between 1 and 65535.
+--debug	If set to true, creates endlpoints /app/log/access/ which returns
+            a JSON access log from the database and /app/error which throws 
+            an error with the message "Error test successful." Defaults to 
+            false.
+--log		If set to false, no log files are written. Defaults to true.
+            Logs are always written to database.
+--help	Return this message and exit.
+`)
+app.use( (req, res, next) => {
+    let logdata = {
+        remoteaddr: req.ip,
+        remoteuser: req.user,
+        time: Date.now(),
+        method: req.method,
+        url: req.url,
+        protocol: req.protocol,
+        httpversion: req.httpVersion,
+        status: res.statusCode,
+        referer: req.headers['referer'],
+        useragent: req.headers['user-agent']
+    }
+    console.log(logdata)
+    const stmt = db.prepare('INSERT INTO accesslog (remoteaddr, remoteuser, time, method, url, protocol, httpversion, status, referer, useragent) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)')
+    const info = stmt.run(logdata.remoteaddr, logdata.remoteuser, logdata.time, logdata.method, logdata.url, logdata.protocol, logdata.httpversion, logdata.status, logdata.referer, logdata.useragent)
+    next();
+  });
+
 
 
 
